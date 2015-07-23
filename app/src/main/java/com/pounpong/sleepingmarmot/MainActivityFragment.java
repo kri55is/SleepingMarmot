8b26a8b8209ca6ea9b3413d7c3398fe9c1a570ac
kri55is <brisseau.emilie@gmail.com>
2015-07-23 10:38:17 -0700

    app working. still 2 small bugs :
    1- you put to sleep, you close the app,
       restart the app and it will propose to sleep again instead of wake up.
    2- after a click the button has to be unaccessible until all actions are done
       ( mostly for the connection or deco to internet that takes long).

---
+++ app/src/main/java/com/pounpong/sleepingmarmot/MainActivityFragment.java
@@ -0,0 +1,195 @@
+package com.pounpong.sleepingmarmot;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.media.AudioManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ToggleButton;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+    private final String TAG = "MainActivityFragment";
+
+    ToggleButton btnSwitchSleep;
+    boolean isSwitchSleepBtnClicked = false;
+
+    AudioManager audioManager;
+    WifiManager wifiManager;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
+        //Remove title bar
+        btnSwitchSleep = (ToggleButton) v.findViewById(R.id.btnSwitchSleep);
+        btnSwitchSleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeSwitchSleepStatus();
+
+            }
+        });
+
+        audioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+        if(audioManager == null){
+            Log.e(TAG, "No audioManager found");
+        }
+        wifiManager = (WifiManager) getActivity().getSystemService(Context.WIFI_SERVICE);
+        if(wifiManager == null){
+            Log.e(TAG, "No wifiManager found");
+        }
+
+        return v;
+    }
+
+    private void changeSwitchSleepStatus(){
+        if (isSwitchSleepBtnClicked) {
+            isSwitchSleepBtnClicked = false;
+            btnSwitchSleep.setChecked(false);
+            wakeUp();
+        }
+        else {
+            isSwitchSleepBtnClicked = true;
+            btnSwitchSleep.setChecked(true);
+            sleep();
+        }
+    }
+
+    private void sleep(){
+        mute();
+        stopWifi();
+        stopCellularData();
+    }
+
+    private void wakeUp(){
+        unMute();
+        startWifi();
+        startCellularData();
+    }
+
+    private void mute(){
+        if(audioManager != null) {
+            int state = audioManager.getRingerMode();
+            if (state != audioManager.RINGER_MODE_VIBRATE)
+                audioManager.setRingerMode(audioManager.RINGER_MODE_VIBRATE);
+        }
+    }
+    private void unMute(){
+        if(audioManager != null) {
+            int state = audioManager.getRingerMode();
+            if (state != audioManager.RINGER_MODE_NORMAL)
+                audioManager.setRingerMode(audioManager.RINGER_MODE_NORMAL);
+        }
+    }
+
+    private boolean stopWifi(){
+        boolean result = false;
+        if(wifiManager != null){
+            try {
+                int state = wifiManager.getWifiState();
+                if ((state != wifiManager.WIFI_STATE_DISABLED) && (state != wifiManager.WIFI_STATE_DISABLING))
+                    result = wifiManager.setWifiEnabled(false);
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+    private boolean startWifi(){
+        boolean result = false;
+        try {
+            int state = wifiManager.getWifiState();
+            if((state != wifiManager.WIFI_STATE_ENABLED) && (state != wifiManager.WIFI_STATE_ENABLING))
+                result = wifiManager.setWifiEnabled(true);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+
+    }
+
+    private void startCellularData(){
+        //setMobileDataEnabled(true);
+        //setMobileDataState(true);
+    }
+
+    private void stopCellularData(){
+        //setMobileDataEnabled(false);
+        //setMobileDataState(false);
+    }
+/*
+    public void setMobileDataState(boolean mobileDataEnabled)
+    {
+        try
+        {
+            Context context = getActivity();
+            TelephonyManager telephonyService = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+            Method setMobileDataEnabledMethod = telephonyService.getClass().getDeclaredMethod("setDataEnabled", boolean.class);
+
+            if (null != setMobileDataEnabledMethod)
+            {
+                setMobileDataEnabledMethod.invoke(telephonyService, mobileDataEnabled);
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.e(TAG, "Error setting mobile data state", ex);
+        }
+    }*/
+/*
+    private void setMobileDataEnabled(boolean enabled) {
+
+        Context context = getActivity();
+        final ConnectivityManager conman =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        try {
+            final Class conmanClass = Class.forName(conman.getClass().getName());
+            final Field iConnectivityManagerField = conmanClass.getDeclaredField("mService");
+            iConnectivityManagerField.setAccessible(true);
+            final Object iConnectivityManager = iConnectivityManagerField.get(conman);
+            final Class iConnectivityManagerClass = Class.forName(
+                    iConnectivityManager.getClass().getName());
+            final Method setMobileDataEnabledMethod = iConnectivityManagerClass
+                    .getDeclaredMethod("setMobileDataEnabled", Boolean.TYPE);
+            setMobileDataEnabledMethod.setAccessible(true);
+
+            setMobileDataEnabledMethod.invoke(iConnectivityManager, enabled);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+*/
+}
