a24e0bc0e93e6783f969705696001655b43ab01e
kri55is <brisseau.emilie@gmail.com>
2015-08-12 15:33:13 -0700

    add action bar + add icon in action bar with the manifest file

--- app/src/main/java/com/pounpong/sleepingmarmot/MainActivity.java
+++ app/src/main/java/com/pounpong/sleepingmarmot/MainActivity.java
@@ -3,6 +3,7 @@ package com.pounpong.sleepingmarmot;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.Window;
 
@@ -17,4 +18,26 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_preferences, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_preferences:
+                //newGame();
+                return true;
+           /*case R.id.help:
+                showHelp();
+                return true;
+                */
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
